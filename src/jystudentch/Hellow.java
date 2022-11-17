884a1983680bee3a7400836342522b8abccff626
jychenhao <2824968806@qq.com>
2022-11-17 20:49:43 +0800

    first

---
+++ src/jystudentch/Hellow.java
@@ -0,0 +1,11 @@
+package jystudentch;
+
+/**
+ * @author shkstart
+ * @create 2022-11-17 20:06
+ */
+public class Hellow {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
